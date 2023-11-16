a361f0fbf65cc738ce5a3938692614a317c3897a
Alejandro Romero <balrking07@gmail.com>
2023-11-16 10:40:15 -0600

    subida del proyecto

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into roles values(default,"USER");
+insert into roles values(default,"ADMIN");
+insert into roles values(default,"DEV");
+
+
+insert into users values(default,"$2a$10$Q86z55cpdRTnnoyX.bEQDOQ63m/ukwtp/ZANa6IFb1kG866Q07xdG","1");
+insert into users values(default,"$2a$10$sXwaTFx6pH9Dig14NV7z/uWYugcoZqVsLxZqZXlvUIJnxs9Oo6EOm","Ramon");
+
+
+insert into user_role values(1,1);
+insert into user_role values(2,2);
